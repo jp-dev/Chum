28b8f2a792a6b3d28d0880dcc78240622493d595
Nick <jpdev.contact@gmail.com>
2021-08-17 08:45:31 +0900

    Change theme from COSMO back to BOOTSTRAP

--- chum.py
+++ chum.py
@@ -11,7 +11,7 @@ import plotly.express as px
 # import pandas as pd
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-external_stylesheets = [dbc.themes.COSMO]
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
