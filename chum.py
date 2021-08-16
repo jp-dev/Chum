e4b492cbd2d194c6068a6944f7dd6cc720c2ce1c
Nick <jpdev.contact@gmail.com>
2021-08-17 08:50:42 +0900

    Changed theme to COSMO. BOOTSTRAP font is ugly

--- chum.py
+++ chum.py
@@ -11,7 +11,7 @@ import plotly.express as px
 # import pandas as pd
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-external_stylesheets = [dbc.themes.BOOTSTRAP]
+external_stylesheets = [dbc.themes.COSMO]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
